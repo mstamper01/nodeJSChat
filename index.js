fea665453426489392c863d3354e0a9fbd445e51
Micah Stamper <mstamper01@gmail.com>
2015-09-28 19:17:16 -0400

    Updated for socket changes on box index.js and .html

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ io.on('connection', function(socket){
 	});
 });
 
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		consolge.log('message: ' + msg);
+	});
+});
+
+
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
